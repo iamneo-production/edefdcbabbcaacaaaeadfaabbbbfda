9a4ef984d90783c880db59fdce245a3d8b858e68
Iamneo <techadmin@iamneo.ai>
2023-10-06 10:23:57 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/HelloController.java
+++ springapp/src/main/java/com/examly/springapp/HelloController.java
@@ -1,1 +1,14 @@
-public
+package com.examly.springapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController{
+
+    @GetMapping
+    public String hello(){
+        return "Good guys";
+    }
+}
