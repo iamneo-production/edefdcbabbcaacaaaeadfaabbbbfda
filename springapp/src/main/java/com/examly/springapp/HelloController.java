118e80e74cce1934cdb8a0c07c7638e8e87048cc
Iamneo <techadmin@iamneo.ai>
2023-10-06 10:18:57 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/HelloController.java
+++ springapp/src/main/java/com/examly/springapp/HelloController.java
@@ -1,11 +1,1 @@
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class HelloController {
-
-@GetMapping
- public String hello(){
-    return "Hello guys";
- }    
-}
+public
